3672333a9756e9c0901054e0f10a1798e27d2a4b
Abdisa.dev <meetabdisa@gmail.com>
2025-10-11 09:58:51 +0300

    set up the routes

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -1,11 +1,44 @@
-import React from "react";
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import Income from "./pages/Dashboard/Income";
+import Expense from "./pages/Dashboard/Expense";
+import Home from "./pages/Dashboard/Home";
 const App = () => {
   return (
     <>
-      <div className="text-3xl text-purple-600">App</div>
+      <div>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Root />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/signup" element={<SignUp />}></Route>
+            <Route path="/dashboard" element={<Home />}></Route>
+            <Route path="/income" element={<Income />}></Route>
+            <Route path="/expense" element={<Expense />}></Route>
+          </Routes>
+        </Router>
+      </div>
     </>
   );
 };
 
 export default App;
+
+const Root = () => {
+  //check if token exists in localStorage
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  //redirect to dahsboard if authenticated,otherwise to login
+
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  );
+};
