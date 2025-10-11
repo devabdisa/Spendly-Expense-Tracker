26390b805feacd56018baac0e35b6cc54cad53f8
Abdisa.dev <meetabdisa@gmail.com>
2025-10-11 20:24:10 +0300

    create Profile Photo Selector Component

--- frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import AuthLayout from "../../components/Layout/AuthLayout";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
@@ -32,7 +31,7 @@ const Login = () => {
 
   return (
     <AuthLayout>
-      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify center">
+      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
         <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
         <p className="text-xs text-slate-700 mt-[5px] mb-6">
           {" "}
