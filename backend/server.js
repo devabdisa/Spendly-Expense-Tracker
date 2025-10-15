d50f2bc544a35181f5e3e1df42d2f2951908d043
Abdisa.dev <meetabdisa@gmail.com>
2025-10-15 21:12:45 +0300

    authenticaton api register user

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
-require("dotenv").config();
 const express = require("express");
+require("dotenv").config();
 const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 
 //Middleware to handle CORS
@@ -17,6 +19,7 @@ app.use(
 app.use(express.json());
 
 connectDB();
+app.use("/api/v1/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
