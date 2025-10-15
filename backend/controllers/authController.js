7ceed9d851abeab8d58dbba4332ed3b5406c597a
Abdisa.dev <meetabdisa@gmail.com>
2025-10-15 22:50:06 +0300

    check user authentication api

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -76,4 +76,18 @@ exports.loginUser = async (req, res) => {
 };
 
 //Check user
-exports.getUserInfo = async (req, res) => {};
+exports.getUserInfo = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+
+    if (!user) {
+      return res.status(404).json({ message: "user not found" });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error registering user", error: error.message });
+  }
+};
