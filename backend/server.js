cc31cf9ebfd9ba37580875aac30271b6684a8e76
Abdisa.dev <meetabdisa@gmail.com>
2025-10-15 23:57:04 +0300

    image upload api

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,15 @@ app.use(express.json());
 connectDB();
 app.use("/api/v1/auth", authRoutes);
 
+
+
+
+
+//Serve uploads folder 
+
+app.use("/uploads",express.static(path.join(__dirname,"uploads")))
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
