144b5fa6d09459f033ca60055727606329f1b01a
Abdisa.dev <meetabdisa@gmail.com>
2025-10-18 22:16:28 +0300

    Create Dashboard APIs

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const incomeRoutes = require("./routes/incomeRoutes");
 const expenseRoutes = require("./routes/expenseRoutes");
-
+const dashboardRoutes = require("./routes/dashboardRoutes");
 
 const app = express();
 
@@ -15,7 +15,7 @@ app.use(
   cors({
     origin: process.env.CLIENT_URL || "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeader: ["content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
@@ -25,14 +25,11 @@ connectDB();
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/income", incomeRoutes);
 app.use("/api/v1/expense", expenseRoutes);
+app.use("/api/v1/dashboard", dashboardRoutes);
 
+//Serve uploads folder
 
-
-
-
-//Serve uploads folder 
-
-app.use("/uploads",express.static(path.join(__dirname,"uploads")))
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
 
