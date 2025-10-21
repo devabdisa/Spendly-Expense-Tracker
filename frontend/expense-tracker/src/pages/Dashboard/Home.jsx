b5376331afa1de2e296bb59baca16572954b87dd
Abdisa.dev <meetabdisa@gmail.com>
2025-10-21 23:25:27 +0300

    Create the Dashboard page layout

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import DashboardLayout from "../../components/Layout/DashboardLayout";
+import { useUserAuth } from "../../hooks/useUserAuth";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
 
-export default Home
+  useUserAuth();
+  return (
+    <DashboardLayout activeMenu="Dashboard">
+      <div className="my-5 mx-auto">
+        Home
+      </div>
+    </DashboardLayout>
+  );
+};
+
+export default Home;
