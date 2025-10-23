22fe22fa3e62980fe673147b8b51bb2736354873
Abdisa.dev <meetabdisa@gmail.com>
2025-10-23 23:06:55 +0300

    add prepareIncomeBarChartData on helper.js

--- frontend/expense-tracker/src/utils/helper.js
+++ frontend/expense-tracker/src/utils/helper.js
@@ -1,3 +1,5 @@
+import moment from 'moment'
+
 export const validateEmail = (email) => {
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return regex.test(email);
@@ -28,8 +30,23 @@ export const addThousandsSeparator = (num) => {
 
 export const prepareExpenseBarCharData = (data = []) => {
   const chartData = data.map((item) => ({
+    month: moment(item?.date).format("Do MMM"),
     category: item?.category,
     amount: item?.amount,
   }));
+  return chartData;
+};
+
+export const prepareIncomeBarChartData = (data = []) => {
+  const sortedData = [...data].sort(
+    (a, b) => new Date(a.date) - new Date(b.date)
+  );
+
+  const chartData = sortedData.map((item) => ({
+    month: moment(item?.date).format("Do MMM"),
+    amount: item?.amount,
+    source: item?.source,
+  }));
+
   return chartData;
 };
